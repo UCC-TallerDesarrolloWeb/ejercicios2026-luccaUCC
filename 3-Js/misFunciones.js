b0388c0d2e0fb707158f5b6c03c03a623eac2f49
Lucca <2516093@ucc.edu.ar>
2026-09-21 08:41:31 -0300

    feat: grados a radianes

--- 3-Js/misFunciones.js
+++ 3-Js/misFunciones.js
@@ -52,3 +52,23 @@ function convertirUnidades(id, valor){
     }
 
 }
+
+/**
+ * Convertor grados a radianes
+ * @method convertirGR
+ * @param {string} id - id del elemento input en el html
+ */
+
+function convertirGR(id){
+    let grad, rad;
+
+    if(id==="grados"){
+        grad= document.getElementById("grados").value;
+        rad= grad*Math.PI/180;
+    }else{
+        rad= document.getElementById("radianes").value;
+        grad= rad*180/Math.PI;
+    }
+    document.getElementById("grados").value = grad;
+    document.getElementById("radianes").value = rad;
+}
