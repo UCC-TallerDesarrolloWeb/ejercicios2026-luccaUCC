4fc5f9625d7432bf66da359f54229d4212656dd8
Lucca <2516093@ucc.edu.ar>
2026-09-21 08:02:20 -0300

    feat conversor unidades

--- 3-Js/misFunciones.js
+++ 3-Js/misFunciones.js
@@ -5,3 +5,51 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function convertirUnidades(id, valor){
+
+    if(isNaN(valor)){
+        alert("se ingreso un valor incorrecto donde deberia haber "+id); 
+
+        document.getElementById("metro").value = "" ; 
+
+        document.getElementById("pulgada").value = "" ; 
+
+        document.getElementById("pie").value = "" ; 
+
+        document.getElementById("yarda").value = "";
+
+    } else if(id === "metro"){
+        document.getElementById("pulgada").value = valor * 39.3701; 
+
+        document.getElementById("pie").value = valor * 3.28084 ; 
+
+        document.getElementById("yarda").value = valor * 1.09361; 
+
+    } else if(id === "pulgada"){
+
+        document.getElementById("metro").value = valor * 0.0254
+
+        document.getElementById("pie").value = valor * 0.08333333
+
+        document.getElementById("yarda").value = valor * 0.02777778
+
+    } else if(id === "pie"){
+
+        document.getElementById("metro").value = valor * 0.3048
+
+        document.getElementById("yarda").value = valor * 0.3333333
+
+        document.getElementById("pulgada").value = valor * 12; 
+        
+    } else if(id === "yarda"){
+
+        document.getElementById("metro").value = valor * 0.9144
+
+        document.getElementById("pulgada").value = valor * 36; 
+
+        document.getElementById("pie").value = valor * 3; 
+        
+    }
+
+}
